4c6588d8d4b45ccae28455e748b4404f3acc5028
pedrohpso <pedro.cobalt@gmail.com>
2021-10-12 19:59:39 -0300

    Tabelas finalizadas

---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Login.css";
+
+const Login = ({props}) => {
+    return(
+        <div className="conteudo">
+             <h1>Login</h1>
+        </div>
+    )
+}
+export default Login;
