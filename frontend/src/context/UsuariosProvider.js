bbcb6a046d194606dafddbc30bd452772636cfb5
pedrohpso <pedro.cobalt@gmail.com>
2021-12-01 23:29:19 -0300

    atualização nos providers, tentativa de adicionar Submit handler

--- frontend/src/context/UsuariosProvider.js
+++ frontend/src/context/UsuariosProvider.js
@@ -8,19 +8,34 @@ const UsuariosProvider = (props) =>{
         setUsuarios(
             [
                 {
-                    codigo: 0,
+                    id: 0,
                     nome_completo: "Higor Rangel",
                     email: "hirgo@gmail.com",
                     contato: "12123451234",
-                    Cargo: "Administrador",
+                    cargo: "Administrador",
                     situacao: "Ativo",
                 },
             ]
         )
     }, [])
     
+    const onUsuarioSubmit = (event) =>{
+        event.preventDefault();
+        let newUsuarios = [...usuarios,
+        {
+          id:usuarios.length,
+          nome_completo: (event.target.usuarioNome.value + " " + event.target.usuarioNomeMeio.value+ " " + event.target.usuarioNomeUltimo.value),
+          email: event.target.usuarioEmail.value,
+          contato: event.target.usuarioContato.value,
+          cargo: event.target.usuarioCargo.value,
+          situacao: event.target.usuarioSituacao.value,
+        }
+        ];
+        setUsuarios(newUsuarios);
+      }
+
     return(
-        <UsuariosContext.Provider value={{ usuarios: usuarios}}>
+        <UsuariosContext.Provider value={{ usuarios: usuarios, onUsuarioSubmit:onUsuarioSubmit}}>
             {props.children}
         </UsuariosContext.Provider>
     )
