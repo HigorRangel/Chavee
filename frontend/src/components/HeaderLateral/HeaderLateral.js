d83c7dbbc3dadb20aafa8f872f2d715fd1cc01c3
Higor Rangel <higorrg2000@hotmail.com>
2021-12-01 21:06:07 -0300

    Feedback visual dos botões do header lateral

--- frontend/src/components/HeaderLateral/HeaderLateral.js
+++ frontend/src/components/HeaderLateral/HeaderLateral.js
@@ -6,16 +6,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
  
 
 
-function Header(){
+function HeaderLateral(){
     return(
-        <div className="header bg-white">
+        <div className="header bg-white" id="header">
             <nav>
                 <ul className={"list-unstyled"}>
-                    <li className={"mb-5"}><NavLink to="/dashboard" exact><FontAwesomeIcon icon={faHome} color={"#0B2149"}  size="lg"/></NavLink></li>
-                    <li className={"mb-5"}><NavLink to="/chaves" exact><FontAwesomeIcon icon={faKey} color={"#0B2149"}  size="lg"/></NavLink></li>
-                    <li className={"mb-5"}><NavLink to="/emprestimos" exact><FontAwesomeIcon icon={faClipboardList} color={"#0B2149"}  size="lg"/></NavLink></li>
-                    <li className={"mb-5"}><NavLink to="/cargos" exact><FontAwesomeIcon icon={faBriefcase} color={"#0B2149"}  size="lg"/></NavLink></li>
-                    <li><NavLink to="/cadastro-imobiliaria" exact><FontAwesomeIcon icon={faBuilding} color={"#0B2149"}  size="lg"/></NavLink></li>
+                    <li className={"mb-5 botao"}><NavLink to="/dashboard" exact><FontAwesomeIcon icon={faHome} color={"#0B2149"}  size="lg"/></NavLink></li>
+                    <li className={"mb-5 botao"}><NavLink to="/chaves" exact><FontAwesomeIcon icon={faKey} color={"#0B2149"}  size="lg"/></NavLink></li>
+                    <li className={"mb-5 botao"}><NavLink to="/emprestimos" exact><FontAwesomeIcon icon={faClipboardList} color={"#0B2149"}  size="lg"/></NavLink></li>
+                    <li className={"mb-5 botao"}><NavLink to="/cargos" exact><FontAwesomeIcon icon={faBriefcase} color={"#0B2149"}  size="lg"/></NavLink></li>
+                    <li className={"botao"}><NavLink to="/cadastro-imobiliaria" exact><FontAwesomeIcon icon={faBuilding} color={"#0B2149"}  size="lg"/></NavLink></li>
                     {/* <li><NavLink to="/cadastro-cargos" exact><FontAwesomeIcon icon={faHome} color={"#0B2149"}  size="lg"/></NavLink></li>
                     <li><NavLink to="/cadastro-usuario" exact><FontAwesomeIcon icon={faHome} color={"#0B2149"}  size="lg"/></NavLink></li> */}
                 </ul>
@@ -24,4 +24,4 @@ function Header(){
     )
 }
 
-export default Header;
+export default HeaderLateral;
