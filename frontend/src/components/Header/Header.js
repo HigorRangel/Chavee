d55a2666bdb780379bb1d860c7d987d4b64ac67a
pedrohpso <pedro.cobalt@gmail.com>
2021-12-04 14:00:58 -0300

    nome do usuario logado no header e select no cadastro usuario com cargos ativos

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React,{useContext} from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "./Header.css";
 import { faBriefcase, faHome, faKey, faBuilding, faClipboardList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {Container, Navbar} from "react-bootstrap"
 import logo from "../../images/Logo.svg";
- 
+import { LoginContext } from "../../context/LoginProvider";
 
 
 function Header(){
     let location = useLocation();
     let mostrar = location.pathname.match("login") ;
-
+    const {token} = useContext(LoginContext);
+    var nome_usuario = "";
+    if(token){
+        nome_usuario = token.primeiro_nome + " " + token.ultimo_nome
+    }
         return(
             mostrar === null && 
                 (<div>
@@ -24,7 +28,7 @@ function Header(){
                         width="170"
                         height="30"
                         className="d-inline-block align-top"
-                        />{' '}
+                        />{' '}<h3>{nome_usuario}</h3>
                     </Navbar.Brand>
                     </Container>
                 </Navbar>
