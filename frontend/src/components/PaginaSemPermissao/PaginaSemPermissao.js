5ba3e5e1219e5d2709ba670cb0ee85503fc8dfe4
Higor Rangel <higorrg2000@hotmail.com>
2021-12-07 20:40:00 -0300

    Criação da página de sem permissão

--- frontend/src/components/PaginaSemPermissao/PaginaSemPermissao.js
+++ frontend/src/components/PaginaSemPermissao/PaginaSemPermissao.js
@@ -1,8 +1,17 @@
+import "./PaginaSemPermissao.css"
+import imagemErro from "../../images/erro-permissao.svg"
+
+
 const PaginaSemPermissao  = (props) => {
     return(
-        <body>
-            <div className="conteudo">
-                <h3>SEM PERMISSÃO</h3>
+        <body id={"body-pagina-s-permissao"}>
+            <div id={"div-conteudo-s-permissao"} className="conteudo">
+                <img src={imagemErro} alt={"Erro"}></img>
+                <h1 id="txt-pg-s-permissao" className="mb-3">SEM PERMISSÃO</h1>
+                <h2 id="txt-s-permissao" className={"text-center"}>
+                    Você não tem permissão para acessar esse recurso. <br />
+                    Entre em contato com seu administrador.
+                </h2>
             </div>
         </body>
     )
